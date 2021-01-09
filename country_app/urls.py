1764987685271dc6d62063b3df7ad8634577e483
Sayef Reyadh <sayef.reyadh.aust@gmail.com>
2021-01-09 17:12:17 +0600

    Details of an specific country

--- country_app/urls.py
+++ country_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('country/<str:a2c>/', views.get_individual_country, name='country_detail'),
     path('populate_country_data/', views.populate_country_data, name='populate_country_data'),
 ]
--- country_app/views.py
+++ country_app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Country
-from .service import country_call
 
 
 # Create your views here.
@@ -9,6 +8,14 @@ from .service import country_call
 def index(request):
     all_country_list = {'all_country': Country.objects.all()}
     return render(request, 'country_app/index.html', context=all_country_list)
+
+
+def get_individual_country(request, a2c):
+    res = Country.objects.filter(alpha2code=a2c)[0]
+    print(res.name , res.population)
+    country = {'individual_country': res}
+    return render(request, 'country_app/get_individual_country.html', context=country)
+
 
 def populate_country_data(request):
     return HttpResponse("Data is fetched and populated by scripts.py")
