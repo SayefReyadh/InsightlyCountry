0ce84199a944a90861bc4772343f15dd7379f173
Sayef Reyadh <sayef.reyadh.aust@gmail.com>
2021-01-09 19:53:53 +0600

    Phase 2: Partial Complete
    create api is created to create a new country.

--- country_app/urls.py
+++ country_app/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('country/<str:a2c>/', views.get_individual_country, name='country_detail'),
-    path('country/delete/<str:a2c>/', views.delete_individual_country, name='delete_country_detail'),
+    path('', views.home, name='home'),
+    path('country/', views.index, name='all_country'),
+    path('country/<str:a2c>/view', views.get_country, name='country'),
+    path('country/delete/<str:a2c>/', views.delete_country, name='delete_country'),
+    path('country/create/', views.create_country, name='create_country'),
+    path('country/create/success', views.success, name='create_country_success'),
     path('reset/', views.populate_country_data, name='populate_country_data'),
 ]
--- country_app/views.py
+++ country_app/views.py
@@ -6,23 +6,45 @@ from script import bulk_create_all_country
 
 # Create your views here.
 
+def home(request):
+    return render(request, 'country_app/home.html')
+
+
 def index(request):
     all_country_list = {'all_country': Country.objects.all()}
-    return render(request, 'country_app/index.html', context=all_country_list)
+    return render(request, 'country_app/all_country.html', context=all_country_list)
 
 
-def get_individual_country(request, a2c):
+def get_country(request, a2c):
     country = Country.objects.filter(alpha2code=a2c)
     if country:
         country = {'individual_country': Country.objects.filter(alpha2code=a2c)[0]}
-        return render(request, 'country_app/get_individual_country.html', context=country)
+        return render(request, 'country_app/single_country.html', context=country)
     else:
         return HttpResponse("Information of Alpha2Code: " + a2c + " has not been found")
 
 
-def delete_individual_country(request, a2c):
+def delete_country(request, a2c):
     Country.objects.filter(alpha2code=a2c).delete()
     return HttpResponse("Information of Alpha2Code: " + a2c + " has been deleted")
+
+
+def create_country(request):
+    return render(request, 'country_app/create_country.html')
+
+
+def success(request):
+    Country.objects.create(
+        alpha2code=request.POST["alpha2code"],
+        name=request.POST["name"],
+        capital=request.POST["capitol"],
+        population=int(request.POST["population"]),
+        timezones=request.POST["timezones"],
+        flag=request.POST["flag"],
+        language_list=request.POST["language"],
+        neighbour_list=request.POST["neighbouring"],
+    )
+    return HttpResponse("Information Added")
 
 
 def populate_country_data(request):
