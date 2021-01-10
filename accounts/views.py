268d2de6c84b76ccbc625479e6521e56d8786a2e
Sayef Reyadh <sayef.reyadh.aust@gmail.com>
2021-01-10 21:10:19 +0600

    User Authenticated Complete

--- accounts/views.py
+++ accounts/views.py
@@ -13,36 +13,41 @@ class CreateUserForm(UserCreationForm):
 
 
 def register_page(request):
-    form = CreateUserForm()
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Account Has Been Created")
+                return redirect('login')
+            else:
+                print("Not Valid")
 
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Account Has Been Created")
-            return redirect('login')
-        else:
-            print("Not Valid")
-
-    context = {'form': form}
-    return render(request, 'accounts/register.html', context)
+        context = {'form': form}
+        return render(request, 'accounts/register.html', context)
 
 
 def login_page(request):
-    if request.method == 'POST':
-        user = request.POST.get('username')
-        passw = request.POST.get('password')
-        print(user, passw)
-        user = authenticate(request, username=user, password=passw)
-
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.info(request, 'Username OR Password is Incorrect')
-            return redirect('login')
+    if request.user.is_authenticated:
+        return redirect('home')
     else:
-        return render(request, 'accounts/login.html')
+        if request.method == 'POST':
+            user = request.POST.get('username')
+            passw = request.POST.get('password')
+            print(user, passw)
+            user = authenticate(request, username=user, password=passw)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username OR Password is Incorrect')
+                return redirect('login')
+        else:
+            return render(request, 'accounts/login.html')
 
 
 def logout_page(request):
--- country_app/views.py
+++ country_app/views.py
@@ -2,20 +2,25 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Country
 from script import bulk_create_all_country
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 
+
+@login_required(login_url='login')
 def home(request):
     all_country_list = {'all_country': Country.objects.all()}
     return render(request, 'country_app/home.html', context=all_country_list)
 
 
+@login_required(login_url='login')
 def index(request):
     all_country_list = {'all_country': Country.objects.all()}
     return render(request, 'country_app/all_country.html', context=all_country_list)
 
 
+@login_required(login_url='login')
 def get_country(request, a2c):
     country = Country.objects.filter(alpha2code=a2c)
     if country:
@@ -25,6 +30,7 @@ def get_country(request, a2c):
         return HttpResponse("Information of Alpha2Code: " + a2c + " has not been found")
 
 
+@login_required(login_url='login')
 def update_country(request, a2c):
     if request.method == 'POST':
         country = Country.objects.filter(alpha2code=a2c)[0]
@@ -35,20 +41,22 @@ def update_country(request, a2c):
         country.language_list = request.POST["language"]
         country.neighbour_list = request.POST["neighbouring"]
         country.save()
-        return redirect('/country/'+a2c+'/view')
+        return redirect('/country/' + a2c + '/view')
     else:
         country = {'individual_country': Country.objects.filter(alpha2code=a2c)[0]}
         return render(request, 'country_app/update_country.html', context=country)
 
 
+@login_required(login_url='login')
 def delete_country(request, a2c):
     Country.objects.filter(alpha2code=a2c).delete()
     return redirect('/country')
     # return HttpResponse("Information of Alpha2Code: " + a2c + " has been deleted")
 
 
+@login_required(login_url='login')
 def create_country(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         Country.objects.create(
             alpha2code=request.POST["alpha2code"],
             name=request.POST["name"],
@@ -64,7 +72,7 @@ def create_country(request):
         return render(request, 'country_app/create_country.html')
 
 
-
+@login_required(login_url='login')
 def populate_country_data(request):
     # Country.objects.all().delete()
     bulk_create_all_country()
